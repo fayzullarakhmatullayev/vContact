4042222697bc4ddfec59ecbf0c7620579a09d2d0
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2023-03-16 14:55:34 +0500

    finilized the app

--- src/components/Form/FormSubmit.vue
+++ src/components/Form/FormSubmit.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="form" @submit.prevent.enter="submitHandler">
+  <form class="form" @submit.prevent.enter="submitHandler" v-if="isEdit">
     <input class="input" placeholder="Name *" required v-model.trim="initialFormValue.name" />
     <input
       class="input"
@@ -19,7 +19,30 @@
     </ul>
     <div class="form-buttons">
       <BaseButton type="button" btn-class="btn-red" @click="emit('closeForm')">Close</BaseButton>
-      <BaseButton type="submit" btn-class="btn-green">{{ isEdit ? 'Edit' : 'Add' }}</BaseButton>
+      <BaseButton type="submit" btn-class="btn-green">Edit</BaseButton>
+    </div>
+  </form>
+  <form class="form" @submit.prevent.enter="submitHandler" v-else>
+    <input class="input" placeholder="Name *" required v-model.trim="addFormValues.name" />
+    <input
+      class="input"
+      placeholder="Phone *"
+      required
+      name="phone"
+      v-model.trim="addFormValues.phone"
+    />
+    <input class="input" placeholder="Email" type="email" v-model.trim="addFormValues.email" />
+    <div class="form-tags">
+      <input class="input" placeholder="Add tag" v-model.trim="tag" />
+      <BaseButton type="button" :btn-class="'btn-blue'" @click="addTags">Add tag</BaseButton>
+    </div>
+
+    <ul class="form-tags__items" v-if="addFormValues.tags && addFormValues.tags.length > 0">
+      <li v-for="(tag, i) in addFormValues.tags" :key="`${tag}_${i}`">#{{ tag }}</li>
+    </ul>
+    <div class="form-buttons">
+      <BaseButton type="button" btn-class="btn-red" @click="emit('closeForm')">Close</BaseButton>
+      <BaseButton type="submit" btn-class="btn-green">Add</BaseButton>
     </div>
   </form>
 </template>
@@ -27,7 +50,7 @@
 <script lang="ts" setup>
 import { useContactsStore } from '@/store/contacts'
 import type { IContacts } from '@/types/contacts'
-import { ref, type PropType } from 'vue'
+import { ref, type PropType, nextTick } from 'vue'
 import BaseButton from '../Ui/Button/BaseButton.vue'
 
 const store = useContactsStore()
@@ -43,21 +66,34 @@ const props = defineProps({
   }
 })
 
+const addFormValues = ref<IContacts>({
+  id: 0,
+  name: '',
+  phone: '',
+  email: '',
+  tags: []
+})
+
 const tag = ref('')
 
 const addTags = () => {
   if (tag.value.length > 0) {
-    props.initialFormValue.tags?.push(tag.value)
+    if (props.isEdit) {
+      props.initialFormValue.tags?.push(tag.value)
+    } else {
+      addFormValues.value.tags?.push(tag.value)
+    }
     tag.value = ''
   }
 }
 
-const submitHandler = () => {
+const submitHandler = async () => {
   if (props.isEdit) {
     store.updateContact(props.initialFormValue)
   } else {
-    store.addContact({ ...props.initialFormValue, id: new Date() })
+    store.addContact({ ...addFormValues.value, id: new Date() })
   }
+  await nextTick()
   emit('closeForm')
 }
 </script>
